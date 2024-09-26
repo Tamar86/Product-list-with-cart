389ab11b10ae3bce8fe0df0b5dad13de90ab709b
Tamar86 <takkokikalishvili@gmail.com>
2024-09-26 22:28:05 +0100

    Updates

--- assets/index-D6m-dzFw.js
+++ assets/index-D6m-dzFw.js
@@ -201,7 +201,6 @@ Error generating stack: `+l.message+`
 
 	${({size:e=Ea})=>{if(e==="small")return Gh;if(e==="medium")return Ea;if(e==="large")return Zh;if(e==="xLarge")return Xh}}
 `;function Or({children:e,variant:t,size:n,onClick:r}){return y.jsx(Jh,{variant:t,size:n,onClick:r,children:e})}const qh=z.ul`
-	max-width: 100%;
 	border-bottom: 0.5px solid hsl(14, 25%, 72%);
 	display: flex;
 	align-items: center;
@@ -236,8 +235,8 @@ Error generating stack: `+l.message+`
 	max-width: 100%;
 	width: 35.5rem;
 	padding-bottom: 3rem;
-	max-height: fit-content;
-	/* height: fit-content; */
+	max-height: 30rem;
+	overflow-y: auto;
 	@media (max-width: 850px) {
 		width: 30.5rem;
 	}
@@ -479,14 +478,14 @@ Error generating stack: `+l.message+`
 	font-weight: 600;
 	color: hsl(14, 65%, 9%);
 `;function L1(){const[e,t]=de.useState(!1),{orderTotal:n}=rn(),{cart:r}=rn();function i(){t(!0)}return y.jsxs(P1,{children:[y.jsxs(_1,{children:[y.jsx(j1,{children:"Order Total"}),y.jsxs(z1,{children:["$",n.toFixed(2)]})]}),y.jsxs(N1,{children:[y.jsx("span",{children:y.jsxs("svg",{xmlns:"http://www.w3.org/2000/svg",width:"21",height:"20",fill:"none",viewBox:"0 0 21 20",children:[y.jsx("path",{fill:"#1EA575",d:"M8 18.75H6.125V17.5H8V9.729L5.803 8.41l.644-1.072 2.196 1.318a1.256 1.256 0 0 1 .607 1.072V17.5A1.25 1.25 0 0 1 8 18.75Z"}),y.jsx("path",{fill:"#1EA575",d:"M14.25 18.75h-1.875a1.25 1.25 0 0 1-1.25-1.25v-6.875h3.75a2.498 2.498 0 0 0 2.488-2.747 2.594 2.594 0 0 0-2.622-2.253h-.99l-.11-.487C13.283 3.56 11.769 2.5 9.875 2.5a3.762 3.762 0 0 0-3.4 2.179l-.194.417-.54-.072A1.876 1.876 0 0 0 5.5 5a2.5 2.5 0 1 0 0 5v1.25a3.75 3.75 0 0 1 0-7.5h.05a5.019 5.019 0 0 1 4.325-2.5c2.3 0 4.182 1.236 4.845 3.125h.02a3.852 3.852 0 0 1 3.868 3.384 3.75 3.75 0 0 1-3.733 4.116h-2.5V17.5h1.875v1.25Z"})]})}),y.jsxs("span",{children:["This is a ",y.jsx(T1,{children:"carbon-neutral"})," delivery"]})]}),y.jsx(Or,{size:"large",variant:"btnConfirm",onClick:i,children:"Confirm Order"}),e&&y.jsx(E1,{cart:r,orderTotal:n})]})}const $1=z.div`
+	max-width: 100%;
 	display: grid;
-	gap: 1.2rem;
 	background-color: hsl(20, 50%, 98%);
 	border-radius: 0.8rem;
 	padding: 1.5rem;
 	gap: 3rem;
-	max-width: 100%;
-	height: fit-content;
+	max-height: 25rem; // Set an appropriate height
+	overflow-y: auto; // Scrollable when items exceed the max-height
 `,I1=z.div`
 	display: flex;
 	flex-direction: column;
